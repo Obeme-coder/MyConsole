feebc08c195fa43583c27c840c71520302d387a3
dima batkovich <gameakaynt220@gmail.com>
2020-09-07 23:00:08 +0300

    Добавьте файлы проекта.

---
+++ MyConsole/Line.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    public interface ILine
+    {
+        string title { get; set; }
+        void Title(bool underCursor);
+        bool Interaction(ConsoleKeyInfo key);
+    }
+    ////////////Button////////////
+    public class Button : ILine
+    {
+        public string title { get; set; }
+        public Action action;
+        public Button(string _title, Action action)
+        {
+            title = _title;
+            this.action = action;
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(title);
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.Enter)
+            {
+                action.Invoke();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    ////////////Link//////////////
+    public class Link : ILine
+    {
+        public string title { get; set; }
+        public IWindow link;
+        public Link(string _title, IWindow _link)
+        {
+            title = _title;
+            link = _link;
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(title);
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            if (key.Key == ConsoleKey.Enter)
+            {
+                App.Forward(link);
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    ////////////Sign//////////////
+    public class Sign : ILine
+    {
+        public string title { get; set; }
+        public Sign(string _title)
+        {
+            title = _title;
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(title);
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            return false;
+        }
+    }
+    ////////////Exit//////////////
+    public class Exit : ILine
+    {
+        public string title { get; set; } = "back";
+        public Exit()
+        {
+
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            if (App.path.Count == 1)
+            {
+                Console.WriteLine("exit");
+            }
+            else
+            {
+                Console.WriteLine(title);
+            }
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            if(key.Key == ConsoleKey.Enter)
+            {
+                App.Back();
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    ////////////RangeFloat//////////////
+    public class RangeFloat : ILine
+    {
+        public string title { get; set; }
+        public Dictionary<string, float> sourse;
+        public string key;
+        public float step;
+        public float min;
+        public float max;
+        public RangeFloat(string _title, Dictionary<string, float> sourse, string key, float value, float _step, float _min, float _max)
+        {
+            title = _title;
+            this.sourse = sourse;
+            this.key = key;
+            sourse[key] = value;
+            step = _step;
+            min = _min;
+            max = _max;
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(title + (sourse[key] - step < min? "  ":" <") + sourse[key].ToString() + (sourse[key] + step > max ? " " : ">"));
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            if(key.Key == ConsoleKey.LeftArrow)
+            {
+                sourse[this.key] = Math.Max(min, sourse[this.key] - step);
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                sourse[this.key] = Math.Min(max, sourse[this.key] + step);
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+    ////////////RangeBool//////////////
+    public class RangeBool : ILine
+    {
+        public string title { get; set; }
+        public Dictionary<string, bool> sourse;
+        public string key;
+        public RangeBool(string _title, Dictionary<string, bool> sourse, string key, bool value)
+        {
+            title = _title;
+            this.key = key;
+            this.sourse = sourse;
+            sourse[key] = value;
+        }
+        public void Title(bool underCursor)
+        {
+            if (underCursor)
+            {
+                Console.BackgroundColor = ConsoleColor.Gray;
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.WriteLine(title + " <" + sourse[key].ToString() + ">");
+        }
+        public bool Interaction(ConsoleKeyInfo key)
+        {
+            if(key.Key == ConsoleKey.LeftArrow)
+            {
+                sourse[this.key] = !sourse[this.key];
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                sourse[this.key] = !sourse[this.key];
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ MyConsole/window.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+
+namespace MyConsole
+{
+    public interface IWindow 
+    {
+        string title { get; set; }
+        void Start();
+        void Exit();
+        void Print();
+        void Interaction(ConsoleKeyInfo key);
+    }
+    /////////////Menu////////////////
+    public class Menu : IWindow
+    {
+        public int cursor { get; set; } = 0;
+        public string title { get; set; }
+        public ILine[] lines { get; set; }
+        public Menu(string _title, params ILine[] _lines)
+        {
+            title = _title;
+            lines = _lines.Concat(new ILine[] { new Exit() }).ToArray();
+        }
+        public void Start()
+        {
+            Print();
+            while (true)
+            {
+                Interaction(Console.ReadKey(true));
+                Print();
+            }
+        }
+        public void Print()
+        {
+            Console.Clear();
+            Console.WriteLine(title.ToUpper() + "\n");
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (false)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+                lines[i].Title(cursor == i);
+                Console.ResetColor();
+            }
+        }
+        public void Interaction(ConsoleKeyInfo key)
+        {
+            if (lines[cursor].Interaction(key))
+            {
+                return;
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                cursor = Math.Min(lines.Length - 1, cursor + 1);
+            }
+            else if (key.Key == ConsoleKey.UpArrow)
+            {
+                cursor = Math.Max(0, cursor - 1);
+            }
+            else if (key.Key == ConsoleKey.Escape)
+            {
+                App.Back();
+            }
+        }
+        public void Exit()
+        {
+        }
+    }
+    ////////////Stream//////////////
+    //class Stream : IWindow
+    //{
+    //    public int cursor { get; set; }
+    //    public string title { get; set; }
+    //    public string path;
+    //    Mutex mtx;
+    //    bool play;
+    //    public Stream(string _title, string _path)
+    //    {
+    //        title = _title;
+    //        path = _path;
+    //        mtx = new Mutex(false, @"Global\" + path.GetHashCode());
+    //    }
+    //    public void Print()
+    //    {
+    //        Thread th = new Thread(Escape_check);
+    //        th.Start();
+    //        play = true;
+    //        while (play)
+    //        {
+    //            mtx.WaitOne();
+    //            string x = File.ReadAllText(path);
+    //            mtx.ReleaseMutex();
+    //            Console.Clear();
+    //            Console.ResetColor();
+    //            Console.WriteLine(title.ToUpper() + "\n");
+    //            Console.WriteLine();
+    //            Console.WriteLine(x);
+    //            Thread.Sleep((int)App.settings["update frequency"]);
+    //        }
+    //    }
+    //    public void Interaction(ConsoleKeyInfo key)
+    //    {
+    //    }
+    //    public void Escape_check()
+    //    {
+    //        while (true)
+    //        {
+    //            if (Console.ReadKey(false).Key == ConsoleKey.Escape)
+    //            {
+    //                Console.Beep();
+    //                play = false;
+    //                App.path.Pop();
+    //                return;
+    //            }
+    //        }
+    //    }
+    //}
+    ////////////Settings//////////////
+}
